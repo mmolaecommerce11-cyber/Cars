aafac276676a18405dd90380570784cea30604f7
WisemanPC <mmolaecommerce11@gmail.com>
2025-12-02 09:00:28 +0200

    Add project files.

---
+++ Cars/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Cars
+{
+    class Cars
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 1, 4, 2 };
+
+            int j = 0;
+
+            while (j < numbers.Length)
+            {
+                Console.WriteLine(numbers[j]);
+                j++;
+            }
+        }
+    }
+}
