17813538211ebc77f865f14e21ca39fcd1e56582
sfortu428-stack <sfortu428@gmail.com>
2025-12-02 09:56:50 +0200

    ho

--- Cars/Program.cs
+++ Cars/Program.cs
@@ -1,22 +1,5 @@
 ﻿using System;
 
-namespace Cars;
-    class Cars
-    {
-        static void Main(string[] args)
-        {
-            int[,] numbers = {{ 1, 4, 2 },{ 5,7,3} };
-
-            int j = 0;
-
-            while (j < numbers.Length)
-            {
-                Console.WriteLine(numbers[0 , j]);
-                j++;
-            }
-        }
-    }
-
 namespace Cars
 {
     class CarsProgram
